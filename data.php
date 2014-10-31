2a534015b28ad746ced6bd77f902700cc11a85bd
David Honess <david.honess+github@gmail.com>
2014-10-31 15:47:28 +0000

    tidy up

--- data.php
+++ data.php
@@ -7,9 +7,10 @@ $time_to = $_GET["to"];
 
 $select_template = "SELECT %s, UNIX_TIMESTAMP(CREATED) as UNIX_TIME FROM WEATHER_MEASUREMENT WHERE CREATED >= FROM_UNIXTIME(%s) AND CREATED <= FROM_UNIXTIME(%s);";
 $select_query = sprintf($select_template, $column_name, $time_from, $time_to);
+
 // Check connection
 if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 $result = mysqli_query($con, $select_query);
@@ -17,8 +18,8 @@ $result = mysqli_query($con, $select_query);
 $rows = array();
 
 while($row = mysqli_fetch_array($result)) {
-	$time_unix = floatval($row['UNIX_TIME']) * 1000; //flot needs it in milliseconds
-	$rows[] = array( $time_unix, floatval($row[$column_name]) );
+    $time_unix = floatval($row['UNIX_TIME']) * 1000; //flot needs it in milliseconds
+    $rows[] = array( $time_unix, floatval($row[$column_name]) );
 }
 
 echo json_encode($rows);
